4786a5ea6eef99b8f1b3ea54ecda916fb2cb1fbe
godlei <1613355752@qq.com>
2026-05-06 13:29:02 +0800

    初始化项目基础框架和通用组件

---
+++ src/main/java/com/godlei/godleiaicodemother/GodleiAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.godlei.godleiaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodleiAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodleiAiCodeMotherApplication.class, args);
+    }
+
+}
