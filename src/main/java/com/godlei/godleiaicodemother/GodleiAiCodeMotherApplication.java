695c404cf607071b606f5a7976cc114d937818a4
godlei <1613355752@qq.com>
2026-05-06 20:09:40 +0800

    添加用户模块和数据库配置

--- src/main/java/com/godlei/godleiaicodemother/GodleiAiCodeMotherApplication.java
+++ src/main/java/com/godlei/godleiaicodemother/GodleiAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.godlei.godleiaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.godlei.godleiaicodemother.mapper")
 public class GodleiAiCodeMotherApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/godlei/godleiaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.godlei.godleiaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.godlei.godleiaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/godlei8">Godlei</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
