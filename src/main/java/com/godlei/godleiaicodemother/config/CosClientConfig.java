19172194053afb8d605b803725cbf32f67376743
godlei <1613355752@qq.com>
2026-05-12 19:15:15 +0800

    腾讯云cos对象存储

--- src/main/java/com/godlei/godleiaicodemother/config/CosClientConfig.java
+++ src/main/java/com/godlei/godleiaicodemother/config/CosClientConfig.java
@@ -1,5 +1,10 @@
 package com.godlei.godleiaicodemother.config;
 
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.region.Region;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -46,13 +51,13 @@ public class CosClientConfig {
      */
     private String bucket;
 
-//    @Bean
-//    public COSClient cosClient() {
-//        // 初始化用户身份信息(secretId, secretKey)
-//        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
-//        // 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
-//        ClientConfig clientConfig = new ClientConfig(new Region(region));
-//        // 生成cos客户端
-//        return new COSClient(cred, clientConfig);
-//    }
+    @Bean
+    public COSClient cosClient() {
+        // 初始化用户身份信息(secretId, secretKey)
+        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+        // 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
+        ClientConfig clientConfig = new ClientConfig(new Region(region));
+        // 生成cos客户端
+        return new COSClient(cred, clientConfig);
+    }
 }
